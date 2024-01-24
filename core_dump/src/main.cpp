8431f7a863445dc3468dd4fa331e7a759c8943cd
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-01-24 13:44:41 +0100

    Update

---
+++ core_dump/src/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+#include <string>
+#include <future>
+#include <vector>
+#include <list>
+#include <mutex>
+#include <memory>
+
+template <typename T>
+void print(T &&t)
+{
+    std::cout << std::forward<T>(t) << std::endl;
+}
+
+void error()
+{
+    int *i = nullptr;
+    *i = 2;
+}
+
+int main()
+{
+    error();
+}
